dec77fbb755cfef6044804810f87993a82d4c630
Victor Recabarren <victormrecabarren@gmail.com>
2019-04-29 23:16:08 -0700

    able to get image src for thumbnail and apply to a card div

--- js/app.js
+++ js/app.js
@@ -1,9 +1,52 @@
+///// establishing query url for ajax
+
+const baseURL = `https://gateway.marvel.com:443/v1/public/characters?`;
+let ts = `1234567654321`;
+const apiKey = `apikey=012c4351d4aecfb2c181fd0b1977192d`;
+const privateKey = `4ed1801c49dd47d9034022152101a0959db7b215
+`;
+const hash = `81f0138cb4590bc6deaacb6e4db98c31`
+let queryType = `name=`
+
+// name that will be decided by user
+let characterName = 'Spider-Man';
+
+//  hash must = `md5 of ts+privateKey+apiKey`
+
+let queryURL = baseURL + `ts=`+ ts + `&` + apiKey + `&` + `hash=` + hash + `&` + queryType
+
+// function that will make call to API
+
+
+
+
+
+
+//// global variable to keep track of dragged node
 let dropped;
 
 $(() => {
+
+
+
+  const getCharacter = (hero) => {
+    $.ajax({
+          url: queryURL + hero
+        }).then((data) => {
+        const imgSrc = (data.data.results[0].thumbnail.path +`.`+ data.data.results[0].thumbnail.extension);
+        $('#card1').css('background-image', `url('${imgSrc}')`)
+      })
+    }
+
+
+  getCharacter('Spider-Man')
+
+
+
+
+
 ///// setting up variables for drag and drop
 
-  const $pic1 = $('#pic1');
   const $slots = $('.slots')
   let pickedUpFrom = ''
   let heldItem = ''
@@ -11,8 +54,9 @@ $(() => {
   ///// drag functions
   const dragStart = (event) => {
     if (event.target !== event.currentTarget) {
-      $('#compareSlot1').css('background', 'lightblue');
-      $('#compareSlot2').css('background', 'lightblue')
+      //change stage slots to indicate dropzone
+      $('#compareSlot1').css('background', 'lightblue').css('border', '2px dashed black');
+      $('#compareSlot2').css('background', 'lightblue').css('border', '2px dashed black');
       // add a class to show card is being held onto
       $(event.target).addClass('hold');
       // make the card invisible in its original slot, but wait a little
@@ -35,7 +79,11 @@ $(() => {
     //check if slot is occupied
     if ($(event.target).attr('class') !== 'fill') {
       // give the slot a new class to show its being hovered over
-      $(event.target).addClass('hovered');
+      if ($(event.target).parent() === 'stage') {
+        $(event.target).addClass('stageHovered');
+      } else {
+        $(event.target).addClass('hovered');
+      }
     }
   }
 
@@ -58,8 +106,9 @@ $(() => {
     /// also set dropped to true to prevent dragEnd from putting
     /// item back into its original slot
     if (event.target.classList.contains('slots')) {
-      $('#compareSlot1').css('background', 'white');
-      $('#compareSlot2').css('background', 'white')
+      //revert stage slots back to normal
+      $('#compareSlot1').css('background', 'white').css('border', '1px black solid');
+      $('#compareSlot2').css('background', 'white').css('border', '1px black solid')
       $(event.target).removeClass('hovered');
       $(event.target).append($(`#${heldItem}`).attr('class', 'fill'));
       dropped = true;
@@ -69,8 +118,9 @@ $(() => {
 
   const dragEnd = (event) => {
     if (event.target !== event.currentTarget) {
-      $('#compareSlot1').css('background', 'white');
-      $('#compareSlot2').css('background', 'white');
+      // revert stage slots back to normal
+      $('#compareSlot1').css('background', 'white').css('border', '1px black solid');
+      $('#compareSlot2').css('background', 'white').css('border', '1px black solid');
       /// check if dropped is true, then change it to false and stop
       if (dropped === true) {
         dropped = false;
