c3c2181de6b9490ccaeeaeb58944c59219b4d118
Victor Recabarren <victormrecabarren@gmail.com>
2019-04-29 18:16:12 -0700

    made dragability of nodes apply to any that come in, fixed bug that prevented drag working when 2 or more pics in DOM

--- js/app.js
+++ js/app.js
@@ -1,73 +1,93 @@
+let dropped;
+
 $(() => {
 ///// setting up variables for drag and drop
 
-  const $fill = $('.fill');
-  const $cards = $('.cards')
-
+  const $pic1 = $('#pic1');
+  const $slots = $('.slots')
+  let pickedUpFrom = ''
+  let heldItem = ''
 
   ///// drag functions
   const dragStart = (event) => {
-    console.log('dragStart');
-    console.log($(event.target).attr('id'));
+    if (event.target !== event.currentTarget) {
+      // add a class to show card is being held onto
+      $(event.target).addClass('hold');
+      // make the card invisible in its original slot, but wait a little
+      // so that you can still "hold" onto the item
+      setTimeout(() => {
+        $(event.target).attr('class', 'invisible')
+      }, 1);
+      // change heldItem to id of item we are holding
+      heldItem = $(event.target).attr('id');
 
-    $(event.target).addClass('hold');
-    setTimeout(() => {
-      $(event.target).attr('class', 'invisible')
-    }, 1);
-
-    const thingImGrabbing = $(event.target).attr('id');
-    console.log(thingImGrabbing);
+      // set pickedUpFrom to the parent of where you took the div from
+      pickedUpFrom = $(event.target).parent().attr('id');
+    }
   }
 
   const dragEnter = () => {
-    console.log('dragEnter');
-    console.log($(event.target).attr('id'));
-    $(event.target).addClass('hovered');
+    //check if slot is occupied
+    if ($(event.target).attr('class') !== 'fill') {
+      // give the slot a new class to show its being hovered over
+      $(event.target).addClass('hovered');
+    }
   }
 
   const dragOver = () => {
-    console.log('dragover');
-    console.log($(event.target).attr('id'));
+    /// this makes the dragDrop function work
     event.preventDefault();
   }
 
   const dragLeave = () => {
-    console.log('dragLeave');
-    console.log($(event.target).attr('id'));
-    $(event.target).attr('class', 'cards')
+    //check if slot is occupied
+    if ($(event.target).attr('class') !== 'fill') {
+      /// set card slot class back to normal
+      $(event.target).attr('class', 'slots')
+    }
   }
 
   const dragDrop = (event) => {
-    console.log('dragDrop');
-    console.log($(event.target).attr('id'));
-
-    // if ($(event.target) === null) {
-    //
-    // }
-
-    $(event.target).append($fill.attr('class', 'fill'));
+    ///// if item dropped on a card slot, then take away the
+    ////  invisible class and append the dragged item to it
+    /// also set dropped to true to prevent dragEnd from putting
+    /// item back into its original slot
+    if (event.target.classList.contains('slots')) {
+      $(event.target).append($(`#${heldItem}`).attr('class', 'fill'));
+      dropped = true;
+    }
   }
+
 
   const dragEnd = (event) => {
-    // console.log('dropped');
-  }
-
+    if (event.target !== event.currentTarget) {
+      /// check if dropped is true, then change it to false and stop
+      if (dropped === true) {
+        dropped = false;
+        return
+      }
+      /// if dropped is false, then return back to original slot
+      $(event.target).attr('class', 'fill');
+      $(`#${pickedUpFrom}`).append($(event.target));
+    }
+}
 
   ///// listeners
+  // loop to set listeners to all card slots
+  for (let i = 0; i < $slots.length; i++) {
+    $slots.eq(i).on('dragover', dragOver);
+    $slots.eq(i).on('dragenter', dragEnter);
+    $slots.eq(i).on('dragleave', dragLeave);
+    $slots.eq(i).on('drop', dragDrop);
+  }
+  // listeners for card
+  for (let i = 0; i < $slots.length; i++) {
+    $slots.eq(i).on('dragstart', dragStart);
+    $slots.eq(i).on('dragend', dragEnd);
 
-  for (let i = 0; i < $cards.length; i++) {
-    $cards.eq(i).on('dragover', dragOver);
-    $cards.eq(i).on('dragenter', dragEnter);
-    $cards.eq(i).on('dragleave', dragLeave);
-    $cards.eq(i).on('drop', dragDrop);
   }
 
-
-
-
-
-
-  $fill.on('dragstart', dragStart)
-  $fill.on('dragend', dragEnd)
+    // $pic2.on('dragstart', dragStart)
+    // $pic2.on('dragend', dragEnd)
 
 })
