d1425daa2dee3d91766c75bc130c31a3f3a38dec
Victor Recabarren <victormrecabarren@gmail.com>
2019-05-09 18:53:51 -0700

    finished basic layout, adding dark mode toggle

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
+
+/// functions
+
+const darkMode = (event) => {
+  // check if dark mode toggle on
+  if (!$('#checkbox').is(':checked')) {
+    // change class of body
+    console.log('dark mode class added to body');
+    $('body').addClass('darkmode');
+
+    //change class of other elements
+    // code here
+
+  } else {
+    // remove dark classes from all elements on page 
+    console.log('dark mode class removed from body');
+    $('body').attr('class', '')
+  }
+};
+
+
+
 $(() => {
-  console.log($);
+  //grabbing nodes
+  const $darkModeButton = $('.slider');
+
+
+// listeners
+  $darkModeButton.on('click', () => {
+    darkMode(event);
+  })
 })
